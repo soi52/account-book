6d5fc9ea57fe6fbef0ae3d9e7ef10e7dd3cf5348
soyeon <soyeon_9605@yu.ac.kr>
2024-02-10 17:51:35 +0900

    Test : Budget - 예산 작성하기

--- budget/src/test/java/com/finance/budget/repository/BudgetRepositoryTest.java
+++ budget/src/test/java/com/finance/budget/repository/BudgetRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.finance.budget.repository;
 
+import com.finance.budget.dto.BudgetRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -9,11 +10,15 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @DisplayName("예산 레파지토리 테스트")
 @MybatisTest
@@ -38,6 +43,24 @@ public class BudgetRepositoryTest {
         // when
         doReturn(2).when(sql).selectOne("Budget.count", date);
         int output = budgetRepository.checkBudget(date);
+
+        // then
+        assertThat(2).isEqualTo(output);
+    }
+
+    @DisplayName("예산 작성하기")
+    @Test
+    public void writeBudget() {
+        // given
+        BudgetRequestDto budgetRequestDto1 = new BudgetRequestDto(1, 3, 10000, Timestamp.valueOf("2024-01-01 09:00:00"));
+        BudgetRequestDto budgetRequestDto2 = new BudgetRequestDto(1, 4, 20000, Timestamp.valueOf("2024-01-01 09:00:00"));
+        List<BudgetRequestDto> budgetRequestDtos = new ArrayList<>();
+        budgetRequestDtos.add(budgetRequestDto1);
+        budgetRequestDtos.add(budgetRequestDto2);
+
+        // when
+        when(sql.insert("Budget.write", budgetRequestDtos)).thenReturn(2);
+        int output = budgetRepository.writeBudget(budgetRequestDtos);
 
         // then
         assertThat(2).isEqualTo(output);
