2880bd230eb499230225fe6f4e7323a6f6228160
soyeon <soyeon_9605@yu.ac.kr>
2024-02-13 02:00:28 +0900

    HotFix : CORS 오류 문제 해결

--- budget/src/main/java/com/finance/budget/config/WebConfig.java
+++ budget/src/main/java/com/finance/budget/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.finance.budget.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 스프링부트 서버에 http://localhost:3000 오리진의 요청에 대하여 CORS를 허용
+        // WebMvcConfigurer를 implements한 WebMvcConfig 클래스를 생성하여 해당 오리진에 대한 CORS를 허용하여 문제 해결
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+    }
 }
