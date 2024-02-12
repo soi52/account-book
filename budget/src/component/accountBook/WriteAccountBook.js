31321cbf80d2f2bf0401b0fbe67833746beff469
soyeon <soyeon_9605@yu.ac.kr>
2024-02-13 02:42:12 +0900

    Feat : 가계부 작성 폼 구현

--- budget/src/component/accountBook/WriteAccountBook.js
+++ budget/src/component/accountBook/WriteAccountBook.js
@@ -4,13 +4,33 @@ import axios_api from '../../config/Axios';
 const WriteAccountBook = () => {
     // 카테고리
     const [categoryBig, setCategoryBig] = useState([]);
+    const [selectCateBig, setSelectCateBig] = useState(null);
     const [categorySmall, setCategorySmall] = useState([]);
 
+    // 큰 카테고리 선택 이벤트 함수
+    const handleCateBigClick = (index) => {
+        setSelectCateBig(index);
+
+        axios_api
+            .get(`account/categorySmall?cateBig=${categoryBig[index]}`)
+            .then(({ data }) => {
+                setCategorySmall(data);
+            })
+            .catch(({ error }) => {
+                console.log('작은 카테고리 불러오기 오류 : ' + error);
+            });
+    };
+
     useEffect(() => {
+        // 큰 카테고리 불러오기
         axios_api
             .get('account/categoryBig')
+            // .then((response) => {
+            //     console.log(response.data);
+            //     console.log(response.status);
+            // })
             .then(({ data }) => {
-                console.log(data);
+                setCategoryBig(data);
             })
             .catch(({ error }) => {
                 console.log('큰 카테고리 불러오기 오류 : ' + error);
@@ -49,6 +69,15 @@ const WriteAccountBook = () => {
             <div className="box-border p-1 m-2 border border-teal-400 border-solid rounded-md">
                 <p className="m-1 font-semibold text-left">✨ 카테고리 ✨</p>
                 <p className="m-1 text-left">🔹 큰 카테고리</p>
+                {categoryBig.map((string, index) => (
+                    <span
+                        key={index}
+                        onClick={() => handleCateBigClick(index)}
+                        className="cursor-default p-0.5 mx-2 hover:bg-cyan-200 border-gray border-2 border-dashed rounded-md"
+                    >
+                        {string}
+                    </span> // 리스트를 순회하며 각 항목을 렌더링
+                ))}
                 <p className="m-1 text-left">🔸 작은 카테고리</p>
             </div>
             <div className="box-border p-1 m-2 border border-teal-400 border-solid rounded-md">
