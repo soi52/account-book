a423899417da855990567e52274bd82d6347db72
soyeon <soyeon_9605@yu.ac.kr>
2024-02-13 02:02:08 +0900

    Set : Main 일부 변경

--- budget/src/main/Main.js
+++ budget/src/main/Main.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../component/login/Login';
+import WriteAccountBook from '../component/accountBook/WriteAccountBook';
+import '../assets/css/main.css';
 
 const Main = () => {
     return (
-        <Routes>
-            <Route path="/login" element={<Login />}></Route>
-        </Routes>
+        <div className="AppMain">
+            <Routes>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/write" element={<WriteAccountBook />}></Route>
+            </Routes>
+        </div>
     );
 };
 
