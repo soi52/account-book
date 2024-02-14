7304ee317746c01835e09e885be14a92edef86b9
soyeon <soyeon_9605@yu.ac.kr>
2024-02-14 20:53:02 +0900

    Modify : Main Css 변경

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { MobileView } from 'react-device-detect';
+import { useLocation } from 'react-router-dom';
 import Main from './main/Main';
 import Header from './main/Header';
 import Footer from './main/Footer';
@@ -7,13 +8,17 @@ import './index.css';
 import '../src/assets/css/main.css';
 
 function App() {
+    const location = useLocation();
+
     return (
         <div className="App">
             <MobileView>
                 <div>
-                    <Header />
+                    {/* Header */}
+                    {location.pathname !== '/' && <Header />}
                     <Main />
-                    <Footer />
+                    {/* Footer */}
+                    {location.pathname !== '/' && <Footer />}
                 </div>
             </MobileView>
         </div>
